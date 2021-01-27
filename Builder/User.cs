7474fd3747f6c4cb003f3b0f3c76364d9eeaafcb
PC-FRAN\Fran <frandiaz1979@gmail.com>
2021-01-27 10:22:38 +0100

    Añadir los ejercicios Decorator y Builder

---
+++ Builder/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    class User
+    {
+        private int phone;
+        private int cp;
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Phone {
+            get 
+            {
+                return phone;
+            }
+            set 
+            { 
+                if (value < 600000000)
+                {
+                    throw new ArgumentException("The phone number is lower than expected.", nameof(Phone));
+                }
+                if (value > 999999999)
+                {
+                    throw new ArgumentException("The phone number is higher than expected.", nameof(Phone));
+                }
+                phone = value;
+            }
+        }
+
+        public int CP
+        {
+            get { return cp; }
+            set
+            {
+                if (value < 1000)
+                {
+                    throw new ArgumentException("The CP number is lower than expected.", nameof(CP));
+                }
+                if (value > 30000)
+                {
+                    throw new ArgumentException("The CP number is higher than expected.", nameof(CP));
+                }
+                cp = value;
+            }
+        }
+
+        public bool IsAdmin { get; set; }
+        public string Name { get; set; }
+        public string Subname { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Decorator/UtilidadesExcel.cs
@@ -0,0 +1,36 @@
+﻿namespace Decorator
+{
+    using System;
+    using System.IO;
+
+    class UtilidadesExcel
+    {
+        public void GenerarFichero(FileStream file, string querySQL, string title)
+        {
+            var data = RequestDataBase(querySQL);
+            CreateExcelFile(file);
+            FillExcelFile(file, data);
+            FormatExcelFile(file, title);
+        }
+
+        private Object RequestDataBase(string consultaSQL)
+        {
+            return null;
+        }
+
+        private void CreateExcelFile(FileStream file)
+        {
+        }
+
+        private void FillExcelFile(FileStream file, Object data)
+        {
+        }
+
+        private void FormatExcelFile(FileStream file, string title)
+        {
+            // Formatea la cabecera de la pagina (fila 1 más ancha, con 1 color, con titulo en letra más grande, etc.)
+            // Formatea los titulos de los campos (fila 2 en negrita con fondo de 1 color)
+            // etc... 
+        }
+    }
+}
